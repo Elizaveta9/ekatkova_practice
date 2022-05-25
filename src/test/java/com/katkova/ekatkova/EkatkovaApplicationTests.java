3a0f789c27e3c8a1e48b8a707abe715c146ab638
Елизавета <katkova_lizochka@bk.ru>
2022-05-25 11:36:05 +0300

    folder structure created

---
+++ src/test/java/com/katkova/ekatkova/EkatkovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katkova.ekatkova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkatkovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
