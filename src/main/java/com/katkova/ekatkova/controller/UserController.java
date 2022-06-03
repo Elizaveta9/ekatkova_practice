1d534dede9bace6d1aba37cfc7935f3fd2e8f93d
Елизавета <katkova_lizochka@bk.ru>
2022-06-03 15:39:58 +0300

    added enum for results

--- src/main/java/com/katkova/ekatkova/controller/UserController.java
+++ src/main/java/com/katkova/ekatkova/controller/UserController.java
@@ -2,12 +2,10 @@ package com.katkova.ekatkova.controller;
 
 import com.katkova.ekatkova.dto.RequestUserRegistration;
 import com.katkova.ekatkova.dto.ResponseResult;
+import com.katkova.ekatkova.service.ResultTypeEnum;
 import com.katkova.ekatkova.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -17,13 +15,8 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseResult registerUser(@RequestBody RequestUserRegistration user){
-        try {
-            userService.saveUser(user);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseResult("error");
-        }
-        return new ResponseResult("success");
+    public ResponseResult registerUser(@RequestBody RequestUserRegistration user) {
+        userService.saveUser(user);
+        return new ResponseResult(ResultTypeEnum.SUCCESS);
     }
 }
--- src/main/java/com/katkova/ekatkova/dto/ResponseResult.java
+++ src/main/java/com/katkova/ekatkova/dto/ResponseResult.java
@@ -1,14 +1,18 @@
 package com.katkova.ekatkova.dto;
 
+import com.katkova.ekatkova.service.ResultTypeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 public class ResponseResult {
     private String result;
+
+    public ResponseResult(ResultTypeEnum result) {
+        this.result = result.getResult();
+    }
 }
